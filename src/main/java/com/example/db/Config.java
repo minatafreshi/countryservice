7e40dc1f1cd4b44d23c9159ccc76c8de1f0b9e9c
mina tafreshi <info.minatafreshi@gmail.com>
2020-09-21 13:28:01 +0330

    Swagger configuration added

--- src/main/java/com/example/db/Config.java
+++ src/main/java/com/example/db/Config.java
@@ -2,6 +2,9 @@ package com.example.db;
 import javax.ws.rs.ApplicationPath;
 
 import com.example.db.controller.CountryController;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -12,5 +15,10 @@ import org.springframework.stereotype.Component;
 public class Config extends ResourceConfig {
     public Config() {
         register(CountryController.class);
+        register(ApiListingResource.class);
+        register(SwaggerSerializers.class);
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1");
+        beanConfig.setResourcePackage("com.example.db.controller");
     }
 }
